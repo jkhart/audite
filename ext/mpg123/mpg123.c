eec5afa714fcb30992bf0c5ec34139d725562628
Ken Hart <jk@jkhart.com>
2013-04-23 21:35:38 -0700

    add logs

--- ext/mpg123/mpg123.c
+++ ext/mpg123/mpg123.c
@@ -13,20 +13,32 @@ void cleanup(mpg123_handle *mh)
 static VALUE rb_cMpg123;
 
 VALUE rb_mpg123_new(VALUE klass, VALUE filename) {
-  printf("Made it: 1");
+  printf("Made it: 1\n");
   int err = MPG123_OK;
   mpg123_handle *mh;
   VALUE mpg123;
   long rate;
   int channels, encoding;
-  printf("Made it: 2");
+  printf("Made it: 2\n");
   Check_Type(filename, T_STRING);
-  printf("Made it: 3");
+  printf("Made it: 3\n");
   if ((mh = mpg123_new(NULL, &err)) == NULL) {
     rb_raise(rb_eStandardError, "%s", mpg123_plain_strerror(err));
   }
-  printf("Made it: 4");
+  printf("Made it: 4\n");
   mpg123_param(mh, MPG123_ADD_FLAGS, MPG123_FORCE_FLOAT, 0.);
+  printf("mpg123_open is: ")
+  printf(mpg123_open(mh, (char*) RSTRING_PTR(filename)));
+  printf("\n");
+
+  printf("mpg123_getformat is: ")
+  printf(mpg123_getformat(mh, &rate, &channels, &encoding));
+  printf("\n");
+
+  printf("MPG123_OK is: ")
+  printf(MPG123_OK)
+  printf("\n")
+
 
   if (mpg123_open(mh, (char*) RSTRING_PTR(filename)) != MPG123_OK ||
       mpg123_getformat(mh, &rate, &channels, &encoding) != MPG123_OK) {
